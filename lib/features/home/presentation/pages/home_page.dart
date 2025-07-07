4a460ff7a592b14db3e4522c7952120cbfbcaff8
sudatra <mail2sudatra@gmail.com>
2025-07-07 19:43:58 +0530

    added posts fetch on home page

--- lib/features/home/presentation/pages/home_page.dart
+++ lib/features/home/presentation/pages/home_page.dart
@@ -1,5 +1,9 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:social_media_app/features/home/presentation/components/custom_drawer.dart';
+import 'package:social_media_app/features/post/presentation/cubits/post_cubit.dart';
+import 'package:social_media_app/features/post/presentation/cubits/post_states.dart';
 import 'package:social_media_app/features/post/presentation/pages/upload_post_page.dart';
 
 class HomePage extends StatefulWidget {
@@ -10,6 +14,23 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  late final postCubit = context.read<PostCubit>();
+
+  @override
+  void initState() {
+    super.initState();
+    fetchAllPosts();
+  }
+
+  void fetchAllPosts() {
+    postCubit.fetchAllPosts();
+  }
+
+  void deletePost(String postId) {
+    postCubit.deletePost(postId);
+    fetchAllPosts();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -26,6 +47,44 @@ class _HomePageState extends State<HomePage> {
         ],
       ),
       drawer: CustomDrawer(),
+      body: BlocBuilder<PostCubit, PostState>(
+        builder: (context, state) {
+          if(state is PostLoading || state is PostUploading) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          } else if(state is PostLoaded) {
+            final allPosts = state.posts;
+            if(allPosts.isEmpty) {
+              return const Center(
+                child: Text("No Posts available!!!"),
+              );
+            }
+
+            return ListView.builder(
+              itemCount: allPosts.length,
+              itemBuilder: (context, index) {
+                final post = allPosts[index];
+
+                return CachedNetworkImage(
+                  imageUrl: post.imageUrl,
+                  height: 430,
+                  width: double.infinity,
+                  fit: BoxFit.cover,
+                  placeholder: (context, url) => const SizedBox(height: 430),
+                  errorWidget: (context, url, error) => const Icon(Icons.error),
+                );
+              },
+            );
+          } else if(state is PostError) {
+            return Center(
+              child: Text(state.message),
+            );
+          } else {
+            return const SizedBox();
+          }
+        },
+      ),
     );
   }
 }
